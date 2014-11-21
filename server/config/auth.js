2165fce3871cfb3f42d5e576e9887eec1601e31d
ndxbxrme <lewis_the_cat@hotmail.com>
2014-11-21 20:58:04 +0000

    callback moved to env

--- server/config/auth.js
+++ server/config/auth.js
@@ -4,21 +4,21 @@ module.exports = {
   facebookAuth: {
     clientID: process.env.FACEBOOK_KEY,
     clientSecret: process.env.FACEBOOK_SECRET,
-    callbackURL: 'http://localhost:9000/api/facebook/callback'
+    callbackURL: process.env.FACEBOOK_CALLBACK
   },
   twitterAuth: {
     consumerKey: process.env.TWITTER_KEY,
     consumerSecret: process.env.TWITTER_SECRET,
-    callbackURL: 'http://localhost:9000/api/twitter/callback'
+    callbackURL: process.env.TWITTER_CALLBACK
   },
   googleAuth: {
     clientID: process.env.GOOGLE_KEY,
     clientSecret: process.env.GOOGLE_SECRET,
-    callbackURL: 'http://localhost:9000/api/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK
   },
   githubAuth: {
     clientID: process.env.GITHUB_KEY,
     clientSecret: process.env.GITHUB_SECRET,
-    callbackURL: 'http://localhost:9000/api/github/callback'
+    callbackURL: process.env.GITHUB_CALLBACK
   },
 };
